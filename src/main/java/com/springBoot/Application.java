e58434cf7f87c2fad942ecc419810c4da1696b2e
chenkuan233 <2366571093@qq.com>
2019-04-14 22:52:22 +0800

    修改

--- src/main/java/com/springBoot/Application.java
+++ src/main/java/com/springBoot/Application.java
@@ -4,6 +4,7 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
@@ -17,8 +18,9 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
  */
 @SpringBootApplication
 @ServletComponentScan
-@EnableScheduling
-@EnableTransactionManagement
+@EnableScheduling // 启用定时
+@EnableAsync // 启用异步调用
+@EnableTransactionManagement // 启用事务管理
 // @PropertySource("classpath:application.yml") //读取指定配置文件
 @MapperScan("com.springBoot.mapper")
 public class Application {
