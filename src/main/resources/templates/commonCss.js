d0b20493f5b7aa7d4146068fb39e5b67e4c35947
chenkuan <2366571093@qq.com>
2019-06-01 00:32:15 +0800

    统一css样式

--- src/main/resources/templates/commonCss.js
+++ src/main/resources/templates/commonCss.js
@@ -1,7 +1,7 @@
 // https://127.0.0.1/
 projectPath = window.location.protocol + "//" + window.location.host + '/';
 
-// 点击样式
-document.write('<link href="' + projectPath + 'css/aLink.css" rel="stylesheet" type="text/css">');
+// 通用样式
+document.write('<link href="' + projectPath + 'css/common.css" rel="stylesheet" type="text/css">');
 // 上传文件样式
 document.write('<link href="' + projectPath + 'css/upload.css" rel="stylesheet" type="text/css">');
